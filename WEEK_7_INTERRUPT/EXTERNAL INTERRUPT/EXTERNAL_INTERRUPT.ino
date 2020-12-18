be94d17be5441b176dd269d55d1aed11a43cf7f9
Nur Aiman Binti Mohd Fuad <73817819+NurAimanMohdFuad@users.noreply.github.com>
2020-12-18 15:46:16 +0800

    Update EXTERNAL_INTERRUPT.ino

--- WEEK_7_INTERRUPT/EXTERNAL INTERRUPT/EXTERNAL_INTERRUPT.ino
+++ WEEK_7_INTERRUPT/EXTERNAL INTERRUPT/EXTERNAL_INTERRUPT.ino
@@ -11,16 +11,16 @@ unsigned char *eicra=(unsigned char*)0x69;
 
 void setup()
 {
-  *ddrb   = 1 <<  5;//Set PB5 as output. The pin which is connecterd to the green LED
-  *sreg  |= (1 <<  7); //The Most Significant Bit to enable or disable interrupts in general. Set this bit to enable the interrupt
-  *eimsk  = 1; //Enable INT0 at pin PD2
-  *eicra  = 1; //Set interrupt mode to "any logic change", Interrupt Sense Control
+  *ddrb   = 1 <<  5;                      //Set PB5 as output. The pin which is connecterd to the green LED
+  *sreg  |= (1 <<  7);                    //The Most Significant Bit to enable or disable interrupts in general. Set this bit to enable the interrupt
+  *eimsk  = 1                             //Enable INT0 at pin PD2
+  *eicra  = 1;                            //Set interrupt mode to "any logic change", Interrupt Sense Control
 }
 
 ISR(INT0_vect)//Interrupt Service Routine
 {
   bool pin_status= ((*pind) &4);
-  *portb=pin_status<<5; //Same as digitalWrite(13, digitalRead(2));
+  *portb=pin_status<<5;                   //Same as digitalWrite(13, digitalRead(2));
 }
 
 void loop()
